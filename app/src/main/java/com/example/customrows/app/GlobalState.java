88036669967bfc88b7a5e39bff2462ae579e9764
yaron na <justame@gmail.com>
2014-06-07 03:43:32 +0300

    push is working

--- app/src/main/java/com/example/customrows/app/GlobalState.java
+++ app/src/main/java/com/example/customrows/app/GlobalState.java
@@ -4,6 +4,10 @@ import android.app.Application;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.PushService;
 
 public class GlobalState extends Application {
 
@@ -12,5 +16,13 @@ public class GlobalState extends Application {
         super.onCreate();
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext()).build();
         ImageLoader.getInstance().init(config);
+
+        Parse.initialize(this, "xVMwKQZ7unJqqW3rIapOd3PRhoBFHYOFLfEXZgn1", "bx9IqOFkorUXcHeixOtd9HKjNx4jR0lIst9cfWo9");
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        if (ParseUser.getCurrentUser() == null) {
+            ParseUser.enableAutomaticUser();
+        };
     }
 }
